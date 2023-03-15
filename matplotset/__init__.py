c51e1264a162c9b4a767b19c784792b17850ad22
Bolun <36321182+BolunDai0216@users.noreply.github.com>
2023-03-15 11:31:38 -0400

    made it a package

---
+++ matplotset/__init__.py
@@ -0,0 +1,1 @@
+from matplotset.core import generate_shape, read_shape, save_shape
--- matplotset/core.py
+++ matplotset/core.py
@@ -2,21 +2,36 @@ import numpy as np
 import pickle
 
 
-def generate_shape(basesize, mean=1.0, std=0.5, low=1, high=6, size=10, nums=1000):
-    rhos = size * basesize
+def generate_shape(
+    basesize,
+    mean=1.0,
+    std=0.5,
+    low=1,
+    high=6,
+    size=10,
+    nums=1000,
+    alt_freq=3,
+    scale=1.0,
+    x_offset=0.0,
+    y_offset=0.0,
+    seed=None,
+):
+    if seed is not None:
+        np.random.seed(seed)
 
-    phis = np.linspace(1e-4, 2 * np.pi, nums)
+    rhos = size * basesize
+    phis = np.linspace(0.0, 2 * np.pi, nums)
     amps = np.random.normal(mean, std, size)
     Ts = np.random.randint(low, high=high, size=size)
 
     for i in range(size):
-        if i % 3 == 0:
+        if i % alt_freq == 0:
             rhos += amps[i] * np.cos(Ts[i] * phis)
         else:
             rhos += amps[i] * np.sin(Ts[i] * phis)
 
-    cart_xs = rhos * np.cos(phis)
-    cart_ys = rhos * np.sin(phis)
+    cart_xs = scale * rhos * np.cos(phis) + x_offset
+    cart_ys = scale * rhos * np.sin(phis) + y_offset
 
     return cart_xs, cart_ys
 
